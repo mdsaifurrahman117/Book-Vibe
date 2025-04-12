13d051b3060b497c038d1f3c5ae267a4f5a01f9c
mdsaifurrahman117 <saifurrahmansaif954@gmail.com>
2025-04-12 21:51:30 +0600

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+            return (
+                        <>
+                                    <Header/>
+                                    <div>
+                                                <h1 className="text-4xl text-center">hello world</h1>
+                                    </div>
+                                    <Footer/>
+                        </>
+            );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css"
+import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const router = createBrowserRouter([
+            {
+                        path: "/",
+                        element: <App/>,
+            },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+            <RouterProvider router={router} />
+);
